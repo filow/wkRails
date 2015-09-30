0dff9f323e3fd612ed2c5a6cc6242e8ad970dd6a
李仁海 <dev@filowlee.com>
2015-09-30 19:53:13 +0800

    admin 模型的测试

--- test/models/manage/admin_test.rb
+++ test/models/manage/admin_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class Manage::AdminTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "管理员密码必须不小于8位" do
+    admin = Manage::Admin.new(name: 'someone', realname: '某用户', password: '211')
+    assert_not admin.save
+    admin.password = '1234567'
+    assert_not admin.save
+    admin.password = '12345678'
+    assert admin.save
+  end
 end
