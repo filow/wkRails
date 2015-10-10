89212c8c3fe662a8f21358380e8b7a233dcdb89e
李仁海 <dev@filowlee.com>
2015-10-10 15:32:41 +0800

    将公共的JS部分移到manage.js中 #55

--- app/assets/javascripts/manage/posts.js
+++ app/assets/javascripts/manage/posts.js
@@ -1,4 +1,4 @@
-//= require 'jquery.icheck'
+
 $(function(){
   // tooltip
   $('.time-column').tooltip();
@@ -34,28 +34,6 @@ $(function(){
     }
   });
 
-  // 构造一个表单向服务器发起请求
-  function sendRequest(href, method, payload){
-    var csrfToken = $('meta[name=csrf-token]').attr('content'),
-        csrfParam = $('meta[name=csrf-param]').attr('content'),
-        form = $('<form method="post" action="' + href + '"></form>'),
-        metadataInput = '<input name="_method" value="' + method + '" type="hidden" />';
-
-    if (csrfParam !== undefined && csrfToken !== undefined) {
-      metadataInput += '<input name="' + csrfParam + '" value="' + csrfToken + '" type="hidden" />';
-    }
-    form.hide().append(metadataInput);
-    for (var i in payload) {
-      if (payload.hasOwnProperty(i)) {
-        var input_element = '<input name="' + i + '" value="' + payload[i] + '" type="hidden" />'
-        form.append(input_element);
-      }
-    }
-    form.appendTo('body');
-    console.log(form);
-    form.submit();
-  }
-
   // 切换隐藏、切换置顶和删除的操作
   $('a.extra-action').click(function (){
     var t = $(this);
@@ -75,6 +53,6 @@ $(function(){
         break;
     }
     return false;
+  });
 
-  })
 });
