ee404742bd29856a08868a4cb0d4040bb1d53941
李仁海 <dev@filowlee.com>
2015-10-12 17:02:46 +0800

    更改用户性别的getter和setter函数，并添加字段翻译

--- app/models/manage/user.rb
+++ app/models/manage/user.rb
@@ -1,3 +1,24 @@
 class Manage::User < ActiveRecord::Base
   mount_uploader :avatar, UserAvatarUploader
+
+  def sex
+    s = super
+    if s.nil?
+      ""
+    elsif s == true
+      "男"
+    else
+      "女"
+    end
+  end
+
+  def sex=(new_sex)
+    if new_sex == '男'
+      super(true)
+    elsif new_sex == '女'
+      super(false)
+    else
+      super(new_sex)
+    end
+  end
 end
