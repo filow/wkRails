6abc9abb0bd88112aae55ba6263d76533efca86a
daikanshan <daikanshan@163.com>
2015-10-13 11:46:05 +0800

    添加用户注册验证

--- app/models/manage/user.rb
+++ app/models/manage/user.rb
@@ -1,3 +1,14 @@
 class Manage::User < ActiveRecord::Base
   mount_uploader :avatar, UserAvatarUploader
+  before_save {self.email = email.downcase}
+  validates :name, presence:true,length:{maximum:10}
+  validates :realname, presence:true,length:{maximum:10}
+  validates :idcard, presence:true
+  validates :phone, presence:true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,length:{maximum:255},
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness:{case_senstive: false}
+  has_secure_password
+  validates :password,length:{minimum:6}
 end
