a476bf2ccf3bc5f3ea1474f5bcd615f45fad6d37
daikanshan <daikanshan@163.com>
2015-10-13 16:00:49 +0800

    修改部分不合理限制

--- app/models/manage/user.rb
+++ app/models/manage/user.rb
@@ -1,11 +1,10 @@
 class Manage::User < ActiveRecord::Base
   mount_uploader :avatar, UserAvatarUploader
-  before_save {self.email = email.downcase}
-  validates :name, presence:true,length:{maximum:10}
-  validates :realname, presence:true,length:{maximum:10}
+  validates :name, presence:true,length:{maximum:30}
+  validates :realname, presence:true,length:{maximum:15}
   validates :idcard, presence:true
   validates :phone, presence:true
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
   validates :email, presence: true,length:{maximum:255},
             format: {with: VALID_EMAIL_REGEX},
             uniqueness:{case_senstive: false}
