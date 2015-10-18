d1af5c45c8a3a991f03fc3baf659cd7169944eae
JasonSi <529511542@qq.com>
2015-10-18 15:19:36 +0800

    cfg模型根据field_type类型来验证数据；基本实现提交设置功能；日期输入框datepicker尚未完成

--- app/controllers/manage/cfg_controller.rb
+++ app/controllers/manage/cfg_controller.rb
@@ -4,23 +4,13 @@ class Manage::CfgController < ManageController
   end
 
   def update
-    if @manage_cfgs.update(cfg_params)
+    @manage_cfg = Cfg.find(params[:cfg_id])
+    if @manage_cfg.update( value: params[:cfg_value])
       redirect_to manage_cfgs_url, notice: '修改成功'
     else
-      render :edit
+      # render :index
+      redirect_to manage_cfgs_url,notice: "#{@manage_cfg.errors[:value].first}"
     end
   end
-
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_manage_cfg
-      @manage_cfg = Cfg.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def manage_cfg_params
-      params.require(:manage_cfg).permit(:value)
-    end
 
 end
--- app/models/cfg.rb
+++ app/models/cfg.rb
@@ -2,6 +2,12 @@ class Cfg < ActiveRecord::Base
   @@config_cache = Cache.new("Config-Cache")
   validates_inclusion_of :field_type,in: %w(string boolean text rich_text date email url number),allow_nil:true,allow_empty:true
 
+  #根据field_type的值来验证value是否合法
+  validates :value, numericality: { only_integer: true ,greater_than_or_equal_to: 0},if: "field_type == 'number'"
+  validates :value, format: {with: /\Ad{4}-d{2}-d{2}/}, if: "field_type == 'date'"
+  validates :value, inclusion: {in: %w(true false)}, if: "field_type == 'boolean'"
+
+
   def self.get(name, force_refresh=false)
     key = name.to_s.upcase
     result = @@config_cache.get(key, nil, force_refresh) do
