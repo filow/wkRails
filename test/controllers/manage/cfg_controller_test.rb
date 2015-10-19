863a3daaa7a9427ad0f4d58ed7d89259cd92a0e0
JasonSi <529511542@qq.com>
2015-10-19 21:08:28 +0800

    修改了点测试，对运行并无影响，仅作备份有时间写测试

--- test/controllers/manage/cfg_controller_test.rb
+++ test/controllers/manage/cfg_controller_test.rb
@@ -1,14 +1,24 @@
 require 'test_helper'
 
 class Manage::CfgControllerTest < ActionController::TestCase
+
+  setup do
+    @cfg = cfgs(:cfgs_one)
+    #登陆
+    @admin = manage_admins(:valid_admin)
+    session[:admin_id] = @admin.id
+    session[:admin_realname] = @admin.realname
+    session[:admin_name] = @admin.name
+  end
+
   test "should get index" do
     get :index
     assert_response :success
   end
 
-  test "should get update" do
-    get :update
-    assert_response :success
-  end
+  # test "更改系统设置" do
+  #   patch :update ,id: cfg.id
+  #   assert_redirected_to manage_cfgs_path
+  # end
 
 end
