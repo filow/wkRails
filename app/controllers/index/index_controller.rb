8e0845bd31602589e53f5130bd3c64006d8a4eb0
李仁海 <dev@filowlee.com>
2015-10-21 17:03:19 +0800

    添加首页模板

--- app/controllers/index/index_controller.rb
+++ app/controllers/index/index_controller.rb
@@ -1,4 +1,5 @@
 class Index::IndexController < IndexController
   def index
+    @posts = Post.valid_posts.order(is_top: :desc, publish_at: :desc, id: :desc).select(:id, :title, :publish_at).limit(6)
   end
 end
