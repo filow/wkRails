de9b56cf3e3f7276eae5874386a03a2ab12efccc
filow <lirenhai427@gmail.com>
2015-11-27 23:33:53 +0800

    重构示例视频部分

--- app/assets/javascripts/index/index.js
+++ app/assets/javascripts/index/index.js
@@ -4,7 +4,7 @@ $(function (){
   $('.video_exp_btn').click(function(){
     var title=$(this).attr('data-title');
     var location=$(this).attr('data-location');
-    $('.video_exp_title').html(title);
+    $('#video_exp_title').text(title);
 
     var flashvars={f: location, c: 0, h: 3,p: 1};
     var params={bgcolor:'#FFF',allowFullScreen:true,allowScriptAccess:'always',wmode:'transparent'};
