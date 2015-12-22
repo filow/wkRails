e12c3a976cfd2e055e4eb11b51d4bead8793d32f
freagle <a17714@yeah.net>
2015-12-22 19:48:21 +0800

    添加评论的审查标志位

--- app/controllers/manage/comment_controller.rb
+++ app/controllers/manage/comment_controller.rb
@@ -1,6 +1,8 @@
 class Manage::CommentController < ManageController
   def index
-    @manage_comments = Manage::CreationComment.order(created_at: :desc).page(params[:page])
+    @unviewed_comments = Manage::CreationComment.where(is_viewed: false).order(created_at: :desc).page(params[:page])
+    @viewed_comments = Manage::CreationComment.where(is_viewed: true, is_hidden: false).order(created_at: :desc).page(params[:page])
+    @hidden_comments = Manage::CreationComment.where(is_hidden: true).order(created_at: :desc).page(params[:page])
   end
 
   def show
