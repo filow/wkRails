e53de84f63b60cdb61f131dcd58ad8657e79082d
freagle <a17714@yeah.net>
2015-12-28 21:01:28 +0800

    后台评论管理show页面

--- app/controllers/manage/comment_controller.rb
+++ app/controllers/manage/comment_controller.rb
@@ -1,16 +1,30 @@
 class Manage::CommentController < ManageController
+  before_action :set_comment, only: [:show, :hide, :destroy]
+
   def index
     @unviewed_comments = Manage::CreationComment.where(is_viewed: false).order(created_at: :desc).page(params[:page])
+  end
+
+  def viewed
     @viewed_comments = Manage::CreationComment.where(is_viewed: true, is_hidden: false).order(created_at: :desc).page(params[:page])
+  end
+
+  def hidden
     @hidden_comments = Manage::CreationComment.where(is_hidden: true).order(created_at: :desc).page(params[:page])
   end
 
   def show
+    @creation = @comment.creation
   end
 
-  def update
+  def hide
   end
 
   def destroy
   end
+
+  private
+    def set_comment
+      @comment = Manage::CreationComment.find(params[:id])
+    end
 end
