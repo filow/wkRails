faaa29db6d301f900c8c9ada48f1e654c765efdf
filow <lirenhai427@gmail.com>
2016-01-19 20:59:04 +0800

    完善资源预编译设置，vendor文件夹只允许白名单文件编译

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+vendor_allowd_files = ['jquery.uploadifive.js', 'animate.css', 'inspinia/style.css']
 Rails.application.config.assets.precompile << Proc.new do |path|
   if path =~ /\.(css|js)\z/
     # if path.split('/')[-1] =~ /^_/
@@ -20,12 +21,15 @@ Rails.application.config.assets.precompile << Proc.new do |path|
     app_assets_path = Rails.root.join('app', 'assets').to_s
     lib_assets_path = Rails.root.join('lib', 'assets').to_s
     vendor_assets_path = Rails.root.join('vendor', 'assets').to_s
-    if full_path.starts_with?(app_assets_path) || full_path.starts_with?(vendor_assets_path) || full_path.starts_with?(lib_assets_path)
+    if full_path.starts_with?(app_assets_path) || full_path.starts_with?(lib_assets_path)
       # puts "including asset: " + full_path
       true
     else
-
-      false
+      if full_path.starts_with?(vendor_assets_path)
+        vendor_allowd_files.include?(path)
+      else
+        false
+      end
     end
   else
     # puts "excluding asset: " + path
