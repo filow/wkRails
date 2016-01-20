791e8d62e8b4e2403559e476b5bec9bb694ef62d
filow <lirenhai427@gmail.com>
2016-01-20 23:00:24 +0800

    在生产环境下设置默认的host和port

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_broadcast_id
 
+  def default_url_options
+    if Rails.env.production?
+      {:host => "filowlee.com", :port => 3456}
+    else
+      {}
+    end
+  end
 
   def set_broadcast_id
     @broadcast_passage = Cfg.get('broadcast_passage')
