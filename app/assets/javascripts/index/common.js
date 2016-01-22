c4555330961412dbf656d844d236f0daa72abdbf
filow <lirenhai427@gmail.com>
2016-01-22 16:26:38 +0800

    将index.js移到Index/common，以避免生产环境下引用失败的问题

--- app/assets/javascripts/index/common.js
+++ app/assets/javascripts/index/common.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require_self
 
 $(document).on('ready page:load', function(){
   Turbolinks.enableProgressBar()
