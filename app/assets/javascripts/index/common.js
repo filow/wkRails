f3902fda5bea31ec9e0d7b00d5c6172c918c1729
filow <lirenhai427@gmail.com>
2016-01-28 21:25:48 +0800

    调整作品详情页的标题、投票按钮

--- app/assets/javascripts/index/common.js
+++ app/assets/javascripts/index/common.js
@@ -18,11 +18,54 @@
 
 $(document).on('ready page:load', function(){
   Turbolinks.enableProgressBar()
-    $(".to_top").click(function(){
-        $('body,html').animate({scrollTop:0},1000);
-        return false;
-    }).hide();
-    $(document).scroll(function() {
-        $(this).scrollTop()==0?$(".to_top").hide():$(".to_top").show();
-    });
+  $(".to_top").click(function(){
+      $('body,html').animate({scrollTop:0},1000);
+      return false;
+  }).hide();
+  $(document).scroll(function() {
+      $(this).scrollTop()==0?$(".to_top").hide():$(".to_top").show();
+  });
+
+  // 投票处理，用于投票的按钮必须有data-id属性，子元素里有.vote_count元素
+  $('.vote').click(function (){
+    var $this = $(this);
+    var id = $this.data('id');
+    var className = $this.attr('class');
+
+    var vote_count_field = $this.find('.vote_count');
+    var origin_text = vote_count_field.text();
+
+    var handler = {
+      cache: false,
+      method: 'delete',
+      statusCode: {
+        401: function () { alert("请您先登录后再进行操作")}
+      },
+      success: function (result) {
+        vote_count_field.text(origin_text);
+        if(result.status) {
+          // 成功
+          vote_count_field.text(result.votes);
+          $this.toggleClass('voted');
+        } else {
+          alert(result.msg);
+        }
+      },
+      error: function (){
+        vote_count_field.text(origin_text);
+      }
+    }
+
+    if ($this.hasClass('voted')) {
+      if(confirm("确认要取消投票吗？")) {
+        vote_count_field.text('...');
+        $.ajax('/creations/' + id + '/vote', handler);
+      }
+    } else {
+      handler.method = 'post';
+      vote_count_field.text('...');
+      $.ajax('/creations/' + id + '/vote', handler);
+    }
+
+  });
 });
